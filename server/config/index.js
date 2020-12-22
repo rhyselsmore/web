d5bdd9f8e6951cf531be79b8ed0d441af494c16f
feedmeapples <aksenov.ro@outlook.com>
2020-12-22 14:22:11 -0800

    Read TLS from config file, remove separate tls yml

--- server/config/index.js
+++ server/config/index.js
@@ -1,9 +1,19 @@
 const { promisify } = require('util');
-const { readFile } = require('fs');
+const { readFile, readFileSync } = require('fs');
 const yaml = require('js-yaml');
 
 let config = undefined;
 const configPath = process.env.TEMPORAL_CONFIG_PATH || './server/config.yml';
+
+const readConfigSync = () => {
+  if (!config) {
+    const cfgContents = readFileSync(configPath, {
+      encoding: 'utf8',
+    });
+    config = yaml.safeLoad(cfgContents);
+  }
+  return config;
+};
 
 const readConfig = async () => {
   if (!config) {
@@ -27,7 +37,7 @@ const getAuthConfig = async () => {
   } = await readConfig();
 
   const auth = {
-    isEnabled: true,
+    enabled: true,
     providers: [
       {
         type,
@@ -45,11 +55,25 @@ const getAuthConfig = async () => {
 
 const getRoutingConfig = async () => {
   let { default_to_namespace } = await readConfig();
-  const routing = { default_to_namespace };
+  const routing = { defaultToNamespace: default_to_namespace };
   return routing;
+};
+
+const getTlsConfig = () => {
+  let { ca, key, cert, server_name, verify_host } = readConfigSync();
+
+  const tls = {
+    ca,
+    key,
+    cert,
+    serverName: server_name,
+    verifyHost: verify_host,
+  };
+  return tls;
 };
 
 module.exports = {
   getAuthConfig,
   getRoutingConfig,
+  getTlsConfig,
 };
